7b0d143fe3847686d1600ef415a9fa878972f1c7
AlyssaScience <alyssa.kalish@tamu.edu>
2022-05-10 11:45:04 +0200

    edit Resize function - should work

--- cpp/Resize.cpp
+++ cpp/Resize.cpp
@@ -8,50 +8,27 @@ using namespace cv;
 using namespace std;
 
 
-Mat resizeMe(Mat im_source, int max_scaleX, int max_scaleY)
+Mat resizeMe(String filename, double scaleX, double scaleY)
 {
-
-	im_source = imread("HappyFish.jpg", IMREAD_UNCHANGED);
+	Mat im_source;
+	im_source = imread(filename, IMREAD_UNCHANGED);
 	// the case if the image is empty 
 	if (im_source.empty())
 	{
 		cout << "Could not open or find the image!\n" << endl;
-		//  cout << "Usage: " << argv[0] << " <Input image>" << endl;
-		return;
+		return im_source;
 	}
 
-	string windowName3 = "Resize Demo";
-	namedWindow(windowName3, WINDOW_AUTOSIZE);
-	imshow(windowName3, im_source);
-	createTrackbar("Value of horizontal axis", windowName3 , &scaleX, max_scaleX , doit , this);
+	Mat result;
+	double width = int(im_source.cols * scaleX);
+	double height = int(im_source.rows * scaleY);
 
-	createTrackbar("Value of vertical axis", windowName3, &scaleY, max_scaleY, doit, this);
+	resize(im_source, result, Size(width, height), INTER_LINEAR);
 
 
-	//create the window
-	namedWindow(windowName3, WINDOW_AUTOSIZE);
-	//show the images 
-	imshow(windowName3, im_source);
-	moveWindow(windowName3, 0, 0);
+	//createTrackbar("Value of horizontal axis", windowName3 , &scaleX, max_scaleX , doit , this);
 
+	//createTrackbar("Value of vertical axis", windowName3, &scaleY, max_scaleY, doit, this);
+
+	return result;
 }
-
-void doit() {
-
-
-	for (float x = 0; x < 2;  x++) 
-		double width = int(x * im_source.cols);
-
-	for (float y = 0; y < 2; y++)
-		double height = int(y * im_source.rows);
-
-}
-
-
-
-resize(im_source, dst, cv::Size(width, height), INTER_LINEAR);
-
-
-
-	
-	
--- h/Resize.h
+++ h/Resize.h
@@ -9,15 +9,4 @@ using namespace std;
 
 
 
-/* def des variable image*/
-	Mat im_source; 
-	Mat dst; 
-	int scaleX;
-	int scaleY;
-	int const max_scaleX = 3; 
-	int const max_scaleY = 3;
-
-Mat resizeMe(Mat im_source, int max_scaleX, int max_scaleY);
-
-
-
+Mat resizeMe(String filename, double scaleX, double scaleY);
